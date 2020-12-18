1a5b6f9d50d19fc7d6c6de5860cc3773ace90fcb
jamirodevulder <31829657+jamirodevulder@users.noreply.github.com>
2020-12-18 12:14:36 +0100

    present finish

---
+++ Project/Assets/present.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class present : MonoBehaviour
+{
+    [SerializeField] private GameObject goodParticles;
+    [SerializeField] private GameObject badParticles;
+    [SerializeField] private int goodHouseLayerNumber;
+    [SerializeField] private int badHouseLayerNumber;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        print(collision.gameObject.layer);
+        if (collision.gameObject.layer == goodHouseLayerNumber)
+        {
+            Instantiate(goodParticles, collision.transform);
+        }
+        if (collision.gameObject.layer == badHouseLayerNumber)
+        {
+            Instantiate(badParticles, collision.transform);
+        }
+        Destroy(gameObject);
+    }
+}
