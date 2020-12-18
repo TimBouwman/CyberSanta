afce8158e0e71941e36fb8446c7aff120a00edcd
Tim Bouwman <tim210201.tb@gmail.com>
2020-12-18 10:15:19 +0100

    added unity assets

---
+++ Project/Assets/Flying/Scripts/ClimbInteractibe.cs
@@ -0,0 +1,22 @@
+﻿//author: Tim Bouwman
+//Github: https://github.com/TimBouwman
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class ClimbInteractibe : XRBaseInteractable
+{
+    protected override void Awake()
+    {
+        base.Awake();
+        onSelectEnter.AddListener(Grab);
+    }
+
+    private void OnDestroy()
+    {
+        onSelectEnter.RemoveListener(Grab);
+    }
+
+    private void Grab(XRBaseInteractor interactor)
+    {
+        
+    }
+}
